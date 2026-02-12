9ba3ef0a6fb0b578673299b84bfe002ac538a671
Navneet Krishna Srivastava <navneetkrishna4@gmail.com>
2026-02-12 15:25:55 +0530

    Update login_page.py >> added logout method

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -2,13 +2,15 @@ from src.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from src.pages.home_page import HomePage
+from src.utils.wait import presence_located, wait_clickable
+
 
 
 class LoginPage(BasePage):
     USERNAME_BOX = (By.ID, "user-name")
     PASSWORD_BOX = (By.ID, "password")
     LOGIN_BTN = (By.ID, "login-button")
-    NAVIGATION_MENU = (By.ID, "react-burger-menu-btn")
     LOGIN_FAIL_ERROR = (By.CSS_SELECTOR, ".error-message-container.error")
     ERROR_MESSAGE = (By.XPATH, "//h3[@data-test= 'error']")
 
@@ -17,7 +19,7 @@ class LoginPage(BasePage):
 
     def is_logged_in(self):
         # to validate login, verify that the navigation icon is available
-        return self.ele_exists(self.NAVIGATION_MENU)
+        return self.ele_exists(HomePage.NAVIGATION_MENU)
 
     def login(self, username="standard_user", password="secret_sauce"):
         if self.is_logged_in():
@@ -33,12 +35,12 @@ class LoginPage(BasePage):
             WebDriverWait(self.driver, 5).until(
                 EC.any_of(
                     EC.visibility_of_element_located(self.LOGIN_FAIL_ERROR),
-                    EC.visibility_of_element_located(self.NAVIGATION_MENU)
+                    EC.visibility_of_element_located(HomePage.NAVIGATION_MENU)
                     # EC.url_contains("inventory.html")
                 )
             )
             # 2. Check which condition actually happened
-            if self.ele_exists(self.NAVIGATION_MENU):
+            if self.ele_exists(HomePage.NAVIGATION_MENU):
                 return "Success"
             return "False"
 
@@ -51,3 +53,29 @@ class LoginPage(BasePage):
         if self.ele_exists(self.LOGIN_FAIL_ERROR):
             return self.driver.find_element(*self.ERROR_MESSAGE).text
         return ""
+
+
+        def log_out(self):
+
+        if not self.is_logged_in():
+            return True
+
+        try:
+
+            presence_located(self.driver, HomePage.NAVIGATION_MENU).click()
+            wait_clickable(self.driver, HomePage.LOGOUT_BTN).click()
+
+            # 1. Wait for EITHER the LOGIN PAGE OR the NAVIGATION MENU
+            WebDriverWait(self.driver, 5).until(
+                EC.any_of(
+                    EC.visibility_of_element_located(self.LOGIN_BTN),
+                    EC.visibility_of_element_located(HomePage.NAVIGATION_MENU)
+                )
+            )
+
+            return self.ele_exists(self.LOGIN_BTN)
+
+        except Exception as e:
+            # 3. Neither happened within 5 seconds (e.g., app crashed or slow network)
+            print(e)
+            return False
